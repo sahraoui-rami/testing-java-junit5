113019ce0b6bf03a434d9e65936c27377881e5b0
ChefServiceInfo <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>
2023-11-17 16:39:32 +0100

    Testing grouped assertions with and without messages.

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Rami SAHRAOUI on 17/11/2023
+ */
+class PersonTest {
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals("Buck", person.getLastName())
+        );
+    }
+    @Test
+    void groupedAssertionsMsgs() {
+        // given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
+                () -> assertEquals("Buck", person.getLastName(), "Last Name Failed")
+        );
+    }
+}
