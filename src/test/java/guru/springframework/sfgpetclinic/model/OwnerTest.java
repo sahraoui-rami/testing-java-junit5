fee2c9a6d055c0da38110ce78e9aa1aae482d2e1
sahraoui-rami <149308696+sahraoui-rami@users.noreply.github.com>
2023-11-18 11:33:22 +0100

    Writing dependent assertions test with JUnit 5

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Rami SAHRAOUI on 18/11/2023
+ */
+class OwnerTest {
+    @Test
+    void dependentAssertions() {
+        // given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        // then
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did Not Match"),
+                        () -> assertEquals("Buck", owner.getLastName(), "Last Name Did Not Match")
+                ),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("1231231234", owner.getTelephone(), "Telephone Did Not Match")
+                )
+        );
+    }
+}
