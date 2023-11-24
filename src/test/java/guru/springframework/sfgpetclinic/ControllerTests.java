6b823c20cf5e9c2e12b2d014a3c302eb853be332
ChefServiceInfo <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>
2023-11-24 16:27:03 +0100

    Besides of the test common properties use case in the previous branch, here we find a solution for the common functionalities with the use of default methods.

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * Created by Rami SAHRAOUI on 24/11/2023
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTests {
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("\n\n############################################");
+        System.out.println("Let's do something here\n\n");
+    }
 }
